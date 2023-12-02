b92d1fc25d5d8dd999c48fc379f248a8d680035f
wlswo <byunjin11@naver.com>
2023-12-02 23:38:45 +0900

    :wastebasket: JWT 도입으로 인해 주석처리

--- src/main/java/com/moco/moco/controller/ProfileController.java
+++ src/main/java/com/moco/moco/controller/ProfileController.java
@@ -8,13 +8,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.moco.moco.config.LoginUser;
-import com.moco.moco.config.auth.SessionUser;
+import com.moco.moco.config.LoginUserInfo;
+import com.moco.moco.config.auth.UserInfo;
 import com.moco.moco.dto.PostListVo;
 import com.moco.moco.service.post.PostService;
 import com.moco.moco.service.profile.ProfileService;
@@ -38,18 +36,18 @@ public class ProfileController {
 		return "profile/profile";
 	}
 
-	@Operation(summary = "회원 정보 수정", description = "회원 프로필과 이름변경 여부를 검사해 변경합니다.")
-	@PostMapping("/{name}/{isImgDelete}")
-	public ResponseEntity ChangeProfile(@PathVariable("name") String name,
-		@RequestParam(value = "image", required = false) MultipartFile multipartFile,
-		@PathVariable("isImgDelete") boolean isImgDelete, @LoginUser SessionUser sessionUser) {
-		Long status = profileService.CheckProfileAndChange(sessionUser, name, multipartFile, isImgDelete);
-		return status == 200L ? ResponseEntity.ok("ok") : ResponseEntity.badRequest().build();
-	}
+	// @Operation(summary = "회원 정보 수정", description = "회원 프로필과 이름변경 여부를 검사해 변경합니다.")
+	// @PostMapping("/{name}/{isImgDelete}")
+	// public ResponseEntity ChangeProfile(@PathVariable("name") String name,
+	// 	@RequestParam(value = "image", required = false) MultipartFile multipartFile,
+	// 	@PathVariable("isImgDelete") boolean isImgDelete, @LoginUserInfo UserInfo sessionUser) {
+	// 	Long status = profileService.CheckProfileAndChange(sessionUser, name, multipartFile, isImgDelete);
+	// 	return status == 200L ? ResponseEntity.ok("ok") : ResponseEntity.badRequest().build();
+	// }
 
 	@Operation(summary = "회원 탈퇴 요청", description = "회원 탈퇴를 요청합니다.")
 	@DeleteMapping("/delete")
-	public ResponseEntity deleteUser(@LoginUser SessionUser sessionUser) {
+	public ResponseEntity deleteUser(@LoginUserInfo UserInfo sessionUser) {
 		userService.deleteUser(sessionUser.getId());
 		return ResponseEntity.ok("탈퇴완료");
 	}
@@ -57,7 +55,7 @@ public class ProfileController {
 	@Operation(summary = "게시글 리스트 요청", description = "자신이 작성한 게시글들을 반환합니다.")
 	@GetMapping("/mypost")
 	public String myPost(@RequestParam(value = "page", defaultValue = "1") Integer pageNum,
-		@LoginUser SessionUser sessionUser, Model model) {
+		@LoginUserInfo UserInfo sessionUser, Model model) {
 		List<PostListVo> boardList = postService.getMyPosts(pageNum, sessionUser.getId());
 		Integer totalPage = postService.getPageList(pageNum);
 
