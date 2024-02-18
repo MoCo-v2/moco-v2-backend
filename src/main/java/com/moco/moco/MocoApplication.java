de1f6d61906f0dd4b56c037a1063b12a88016e45
wlswo <byunjin11@naver.com>
2024-02-18 23:55:55 +0900

    feat : TimeZone 세팅 추가

--- src/main/java/com/moco/moco/MocoApplication.java
+++ src/main/java/com/moco/moco/MocoApplication.java
@@ -1,9 +1,13 @@
 package com.moco.moco;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import jakarta.annotation.PostConstruct;
 
 @EnableJpaAuditing //Base_Entity에 있는 날짜 자동 입력 활성화
 @EnableJpaRepositories(basePackages = {"com.moco.moco.jpaRepository"})
@@ -12,5 +16,10 @@ public class MocoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MocoApplication.class, args);
+	}
+
+	@PostConstruct
+	public void setTimeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 }
